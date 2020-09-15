e17e3684268b10f10a223721447f9af0cb46de96
devwrs <sitewrs@gmail.com>
2020-09-15 22:12:21 +0800

    Fixed import in exaple files

--- example/lib/landscape_player/landscape_player.dart
+++ example/lib/landscape_player/landscape_player.dart
@@ -2,7 +2,6 @@ import 'package:example/utils/mock_data.dart';
 import 'package:flick_video_player/flick_video_player.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:video_player/video_player.dart';
 
 import 'landscape_player_controls.dart';
 
